1b5935a89b3d65bb68e1971e3c438344dc66894a
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2022-07-20 23:51:25 +0200

    [FIX] Login system, starting database usage

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ const PORT = process.env.PORT || 3001;
 const session = require('express-session');
 const passport = require('passport');
 const discordStrategy = require('./strategies/discordStrategy');
+const db = require('./database/database');
+
+db.then(() => {
+    console.log('Connected to database');
+}).catch(err => {
+    console.log(err);
+})
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -21,16 +29,23 @@ app.use(session({
 }));
 
 // Passport
-
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
 
 // Middleware Routes
 app.use('/auth', authRoute);
 
-app.get('*', (req, res) => {
+/*app.get('*', (req, res) => {
     res.send('Not found');
-});
+});*/
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- src/strategies/discordStrategy.js
+++ src/strategies/discordStrategy.js
@@ -5,9 +5,10 @@ passport.use(new DiscordStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: process.env.DISCORD_CALLBACK_URL,
-    scope: ['identify', 'email' ,'guilds', 'guilds.join']
+    scope: ['identify' ,'guilds'],
 }, (accessToken, refreshToken, profile, done) => {
-    console.log(profile);
+    console.log(profile.id);
+    done(null, profile);
 }))
 
 
